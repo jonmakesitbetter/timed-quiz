cba8e55264ee12a2649ebce0d4fabb0bb569e98c
unknown <jonmakesitbetter@gmail.com>
2020-09-07 11:23:02 -0400

    Added framework for next question function

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ function setTime() {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
     startButtonEl.style.display = "none";
+    nextQuestion();
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
       alert("Time!");
     }
 
   }, 1000);
+}
+
+var questions = {
+  question: 'Commonly used data types do not include: ',
+  answers: [
+    {'strings', correct: false},
+    {'booleans', correct: false},
+    {'numbers', correct: false},
+    {'curly braces', correct: true},
+  ]
+}
+
+function nextQuestion(){
+
 }
 
 startButtonEl.addEventListener("click", setTime);
