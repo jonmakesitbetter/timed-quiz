b403db1bdc506b99574e4534ce0304fe9d4a5aec
unknown <jonmakesitbetter@gmail.com>
2020-09-07 12:54:28 -0400

    Created quizObject containing questions array

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var startButtonEl = document.getElementById('start-btn');
 
 var secondsLeft = 90;
 
-var questions = document.getElementById('question');
+var questionsEl = document.getElementById('question-box');
 var answers = document.getElementById('answer-buttons');
 //Timer starter and first question prompt
 function setTime() {
@@ -13,9 +13,7 @@ function setTime() {
     timeEl.textContent = secondsLeft;
     startButtonEl.style.display = "none";
     questionsIndex = 0;
-    nextQuestion();{
-    //Shows first question in the series, starting the looped function below 'nextQuestion'.
-    }
+     
     //Need to add final score prompt to secondsLeft === 0
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
@@ -24,22 +22,27 @@ function setTime() {
 
   }, 1000);
 }
+
 //Questions object containing an array of question objects.
-var questions = {
-  question: 'Commonly used data types do not include: ',
-  answers: [
-    {'strings', correct: false},
-    {'booleans', correct: false},
-    {'numbers', correct: false},
-    {'curly braces', correct: true},
+let questions = {
+  "questionList": ["Commonly used data types do not include: ", "Question 2", "Question 3", "Question 4"],
+
+}
+let quizObject = {
+
+  "questionArray": [
+    "Question 1",
+    "Question 2",
+    "Question 3"
   ]
 }
 
 function nextQuestion(){
+
 //here, we will have the questions index display its current [i] position, thus showing the question and
 //answer buttons. Then log the answers and then set current index to the next one by question[i++], end by
 //calling this function, thus repeating the process with the next question object in the array.
+answerButton.addEventListener("click", console.log("Yep, answer buttons work"));
 }
 
-startButtonEl.addEventListener("click", setTime);
-
+startButtonEl.addEventListener("click", setTime, nextQuestion);
