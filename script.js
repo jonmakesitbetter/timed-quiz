741f86b88d26ed2dad09e82733b1cd73aa3b043d
unknown <jonmakesitbetter@gmail.com>
2020-09-07 11:37:22 -0400

    Added notes and pseudocode, questions object containing questions objects array present, set up looped nextQuestion function.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+//Global variables
 var timeEl = document.querySelector(".time");
 var startButtonEl = document.getElementById('start-btn');
 
 var secondsLeft = 90;
 
+var questions = document.getElementById('question');
+var answers = document.getElementById('answer-buttons');
+//Timer starter and first question prompt
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
     startButtonEl.style.display = "none";
-    nextQuestion();
+    questionsIndex = 0;
+    nextQuestion();{
+    //Shows first question in the series, starting the looped function below 'nextQuestion'.
+    }
+    //Need to add final score prompt to secondsLeft === 0
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
       alert("Time!");
@@ -16,7 +24,7 @@ function setTime() {
 
   }, 1000);
 }
-
+//Questions object containing an array of question objects.
 var questions = {
   question: 'Commonly used data types do not include: ',
   answers: [
@@ -28,7 +36,9 @@ var questions = {
 }
 
 function nextQuestion(){
-
+//here, we will have the questions index display its current [i] position, thus showing the question and
+//answer buttons. Then log the answers and then set current index to the next one by question[i++], end by
+//calling this function, thus repeating the process with the next question object in the array.
 }
 
 startButtonEl.addEventListener("click", setTime);
