0ac647bf59bba9471a2c65554b5fc93c5fc540e9
unknown <jonmakesitbetter@gmail.com>
2020-09-08 21:50:01 -0400

    Added event listener, cleaned code, first question will populate on start button click.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var startButtonEl = document.getElementById("start-button");
 var secondsLeft = 90;
 var score = document.getElementById("score");
 var questionsEl = document.getElementById("question-box");
-var answers = document.getElementById("answer-buttons");
-var questionsIndex = 0; 
-
+var answersEl = document.getElementById("answer-buttons");
+var questionsIndex = 0;
+var answersIndex = 0;
 //Timer starter
 function setTime(event) {
   event.preventDefault();
@@ -31,43 +31,53 @@ var quizArray = [
   {
     question: "Commonly used data types DO NOT include ______.",
     answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-    correct: "Alerts"
+    correct: "Alerts",
   },
   {
-    question: "A very useful tool during development and debugging for printing content to the debugger is _______",
+    question:
+      "A very useful tool during development and debugging for printing content to the debugger is _______",
     answers: ["JavaScript", "Console log", "For loops", "Bash terminal"],
-    correct: "Console log"
+    correct: "Console log",
   },
   {
-    question: "The condition in an if/else statement is enclosed within _______",
+    question:
+      "The condition in an if/else statement is enclosed within _______",
     answers: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
-    correct: "Curly brackets"
+    correct: "Curly brackets",
   },
   {
     question: "Arrays in JavaScript can be used to store _______",
-    answers: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
-    correct: "All of the above"
+    answers: [
+      "Numbers and strings",
+      "Other arrays",
+      "Booleans",
+      "All of the above",
+    ],
+    correct: "All of the above",
   },
-]
-var questionPopulate = document.createElement("h1");
-questionPopulate.textContent = quizArray[questionsIndex].question;
-questionsEl.append(questionPopulate);
-function nextQuestion(){
+];
 
-  if (questionsIndex < quizArray.length){
-var questionPopulate = document.createElement("h1");
-questionPopulate.textContent = quizArray[questionsIndex].question;
-questionsEl.append(questionPopulate);
-    //empty questions & answers 
-    //show question (create tag add content and append)
+function nextQuestion() {
+  if (questionsIndex < quizArray.length) {
+    var questionPopulate = document.createElement("h1");
+    questionPopulate.textContent = quizArray[questionsIndex].question;
+    questionsEl.append(questionPopulate);
+    //empty questions & answers
+    // for (var i = 0; i < quizArray.answers.length; i++){
+    //   var answersPopulate = document.createElement("button");
+    //   answersPopulate.textContent = quizArray[questionsIndex].answers;
+    //   answersEl.append(answersPopulate);
+    // }
  
+    //show question (create tag add content and append)
+
     //loop through answers and add to DOM (create tag add content and append)
-    questionsIndex++; //move to next question
+    questionsIndex++;
+   //move to next question
   } else {
-    console.log("no more questions")
+    console.log("no more questions");
   }
 }
-
 
 //here, we will have the questions index display its current [i] position, thus showing the question and
 //answer buttons. Then log the answers and then set current index to the next one by question[i++], end by
@@ -75,4 +85,5 @@ questionsEl.append(questionPopulate);
 
 // answerButton.addEventListener("click", nextQuestion());
 startButtonEl.addEventListener("click", setTime);
+startButtonEl.addEventListener("click", nextQuestion);
 // answerButtonEl.addEventListener("click", nextQuestion);
